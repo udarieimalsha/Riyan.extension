d359e25ec89595b6c1715e0087a69f8187d3614d
udarieimalsha <udarieimalsha@gmail.com>
2026-03-25 12:39:22 +0530

    Release version 1.0.3: Dynamic versioning in About dialog and manual update check button.

--- Installer/BuildInstaller.ps1
+++ Installer/BuildInstaller.ps1
@@ -2,7 +2,7 @@
 # This script will download Inno Setup if missing and compile the installer.
 
 $issPath = ".\RiyanSetup.iss"
-$outputExe = "..\RiyanSetup_v1.0.2.exe"
+$outputExe = "..\RiyanSetup_v1.0.3.exe"
 
 Write-Host "--- Riyan Revit Tools Installer Builder ---" -ForegroundColor Cyan
 
